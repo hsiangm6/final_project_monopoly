fc044b9fad4d39de56be7f945a249ba54afc42f2
HandsomeRay111 <62639431+HandsomeRay111@users.noreply.github.com>
2022-05-21 15:17:44 +0800

    Update main.c

--- main.c
+++ main.c
@@ -49,6 +49,17 @@ typedef struct Player
 	
 } Player;
 
+typedef struct Building
+{
+	int buyPrice; // The price of occupying this place with a flag.
+	int buildPrice; // The price of building this flag to the structure.
+	int fee; // The fee that the opponent being through be supposed to pay.
+	int buyPriceFromTheOpponent; // The price of this land buying from the opponent.
+	int condition; // The current status of the land, 0 is a flag, while 1 is structure.
+	int owner; // The land is occupied by whom.
+	
+} Building;
+
 void printLand();
 void printPlayerLocation(int p1Location, int p2Location);
 
