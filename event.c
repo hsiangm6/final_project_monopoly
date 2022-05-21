499920e39819fbee8b03cd38d47dd87546cf845a
kh010909 <95903086+kh010909@users.noreply.github.com>
2022-05-21 13:25:06 +0800

    Update event.c

--- event.c
+++ event.c
@@ -1,16 +1,28 @@
 #include "event.h"
-
-int event(Player* p1){
+#include<stdlib.h>
+void event(struct Player* p1){
     switch(p1->location){
         case 5://jail
             p1->gamestatus = -1 ;
         case 2:
         case 11://draw cards
-            draw();
+            draw(&p1);
         case 9://casino
-            casino();
+            casino(&p1);
         case 14://go to start
             p1->location = 0;
             p1->money += 10000;
+            
     }
+}
+
+void draw(struct Player** p1){
+    card = rand() % 10;
+    switch(card){
+        
+    }
+}
+
+void casino(struct Player** p1){
+    
 }
