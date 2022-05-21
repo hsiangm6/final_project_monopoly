694b3c65dd7414cf4ad33fd1127f34fd0b090c74
HandsomeRay111 <62639431+HandsomeRay111@users.noreply.github.com>
2022-05-21 15:27:37 +0800

    Update main.c

--- main.c
+++ main.c
@@ -54,8 +54,8 @@ typedef struct Building
 	int buyPrice; // The price of occupying this place with a flag.
 	int buildPrice; // The price of building this flag to the structure.
 	int finalPrice; // The eventual price of this land.
-	int fee; // The fee that the opponent being through be supposed to pay.
-	int buyPriceFromTheOpponent = 2 * buyPrice; // The price of this land buying from the opponent.
+	int fee = 0.5 * finalPrice; // The fee that the opponent being through be supposed to pay.
+	int buyPriceFromTheOpponent = 2 * finalPrice; // The price of this land buying from the opponent.
 	int condition; // The current status of the land, 0 is a flag, while 1 is structure.
 	int owner; // The land is occupied by whom.
 	
