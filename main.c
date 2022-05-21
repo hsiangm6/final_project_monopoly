84e1fbecbb38d94d9e5e6025197eb857606c6823
HandsomeRay111 <62639431+HandsomeRay111@users.noreply.github.com>
2022-05-21 13:43:45 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char *landmark[] = {"      ", "      ", "  ", "  ", "  ", 
+
+					"      ", "      ", "  ", "  ", "  ",
+					
+					"      ", "      ", "  ", "  ", "  ",
+					
+					"      ", "      ", "  ", "  ", "  ",
+					
+					"      ", "      ", "  ", "  ", "  ",
+
+					"      ", "      ", "  ", "  ", "  ",
+					
+					"      ", "      ", "  ", "  ", "  ",
+					
+					"      ", "      ", "  ", "  ", "  ",
+					
+					"      ", "      ", "  ", "  ", "  ",
+
+					"      ", "      ", "  ", "  ", "  ",
+					
+					"      ", "      ", "  ", "  ", "  ",
+					
+					"      ", "      ", "  ", "  ", "  ",
+					
+					"      ", "      ", "  ", "  ", "  ",
+
+					"      ", "      ", "  ", "  ", "  ",
+					
+					"      ", "      ", "  ", "  ", "  ",
+					
+					"      ", "      ", "  ", "  ", "  ",
+					
+					"      ", "      ", "  ", "  ", "  ",
+					
+					"      ", "      ", "  ", "  ", "  ",
+				    
+                    "                                     ",
+					
+					"******** ******** ******** ******** ******** ********", "********                                     ********" };
+
 typedef struct Player
 {
 	int money;
@@ -9,20 +49,6 @@ typedef struct Player
 	
 } Player;
 
-char *landmark[] = {"      ", "      ", "      ", "      ", "      ", "      ", "      ", "      ", "      ", "      ",
-
-                    "      ", "      ", "      ", "      ", "      ", "      ", "      ", "      ", "      ", "      ",
-
-                    "      ", "      ", "      ", "      ", "      ", "      ", "      ", "      ", "      ", "      ",
-
-                    "      ", "      ", "      ", "      ", "      ", "      ", "      ", "      ", "      ", "      ",
-
-                    "      ", "      ", "      ", "      ", "      ", "      ", "      ", "      ", "      ", "      ",
-
-                    "      ", "      ", "      ", "      ", "                                          ", "                                        ",
-					
-					" ————  ————  ————  ————  ————  ————", " ————                                          ————" };
-
 void printLand();
 void printPlayerLocation(int p1Location, int p2Location);
 
@@ -30,9 +56,9 @@ int main()
 {
 	char nameOfPlayer1[100], nameOfPlayer2[100];
 	printf("Please enter the name of player 1: ");
-    scanf_s("%s", nameOfPlayer1,99);
+    scanf("%s", nameOfPlayer1);
     printf("Please enter the name of player 2: ");
-    scanf_s("%s", nameOfPlayer2,99);
+    scanf("%s", nameOfPlayer2);
 	
 	Player p1 =
     {
@@ -56,137 +82,29 @@ int main()
 
 void printLand()
 {
-    printf("%s\n｜%s｜｜%s｜｜%s｜｜%s｜｜%s｜｜%s｜\n｜%s｜｜%s｜｜%s｜｜%s｜｜%s｜｜%s｜\n｜%s｜｜%s｜｜%s｜｜%s｜｜%s｜｜%s｜\n%s\n",
+    printf("%s\n*%s* *%s* *%s* *%s* *%s* *%s*\n*%s* *%s* *%s* *%s* *%s* *%s*\n*%s%s%s* *%s%s%s* *%s%s%s* *%s%s%s* *%s%s%s* *%s%s%s*\n%s\n",
 
-        landmark[56],
-		landmark[0], landmark[3], landmark[6], landmark[9], landmark[12], landmark[15],
-        landmark[1], landmark[4], landmark[7], landmark[10], landmark[13], landmark[16],
-        landmark[2], landmark[5], landmark[8], landmark[11], landmark[14], landmark[17],
-		landmark[56]);
+        landmark[91],
+		landmark[0], landmark[5], landmark[10], landmark[15], landmark[20], landmark[25],
+        landmark[1], landmark[6], landmark[11], landmark[16], landmark[21], landmark[26],
+        landmark[2], landmark[3], landmark[4], landmark[7], landmark[8], landmark[9], landmark[12], landmark[13], landmark[14], landmark[17], landmark[18], landmark[19], landmark[22], landmark[23], landmark[24], landmark[27], landmark[28], landmark[29],
+		landmark[91]);
 
-    printf("%s\n｜%s｜%s｜%s｜\n｜%s｜%s｜%s｜\n｜%s｜%s｜%s｜\n%s\n", landmark[57], landmark[18], landmark[55], landmark[21], landmark[19], landmark[55], landmark[22], landmark[20], landmark[55], landmark[23], landmark[57]);
-    printf("%s\n｜%s｜%s｜%s｜\n｜%s｜%s｜%s｜\n｜%s｜%s｜%s｜\n%s\n", landmark[57], landmark[24], landmark[55], landmark[27], landmark[25], landmark[55], landmark[28], landmark[26], landmark[55], landmark[29], landmark[57]);
-    printf("%s\n｜%s｜%s｜%s｜\n｜%s｜%s｜%s｜\n｜%s｜%s｜%s｜\n%s\n", landmark[57], landmark[30], landmark[55], landmark[33], landmark[31], landmark[55], landmark[34], landmark[32], landmark[55], landmark[35], landmark[57]);
+    printf("%s\n*%s*%s*%s*\n*%s*%s*%s*\n*%s%s%s*%s*%s%s%s*\n%s\n", landmark[92], landmark[85], landmark[90], landmark[30], landmark[86], landmark[90], landmark[31], landmark[87], landmark[88], landmark[89], landmark[90], landmark[32], landmark[33], landmark[34], landmark[92]);
+    printf("%s\n*%s*%s*%s*\n*%s*%s*%s*\n*%s%s%s*%s*%s%s%s*\n%s\n", landmark[92], landmark[80], landmark[90], landmark[35], landmark[81], landmark[90], landmark[36], landmark[82], landmark[83], landmark[84], landmark[90], landmark[37], landmark[38], landmark[39], landmark[92]);
+    printf("%s\n*%s*%s*%s*\n*%s*%s*%s*\n*%s%s%s*%s*%s%s%s*\n%s\n", landmark[92], landmark[75], landmark[90], landmark[40], landmark[76], landmark[90], landmark[41], landmark[77], landmark[78], landmark[79], landmark[90], landmark[42], landmark[43], landmark[44], landmark[92]);
 
-    printf("%s\n｜%s｜｜%s｜｜%s｜｜%s｜｜%s｜｜%s｜\n｜%s｜｜%s｜｜%s｜｜%s｜｜%s｜｜%s｜\n｜%s｜｜%s｜｜%s｜｜%s｜｜%s｜｜%s｜\n%s\n",
+    printf("%s\n*%s* *%s* *%s* *%s* *%s* *%s*\n*%s* *%s* *%s* *%s* *%s* *%s*\n*%s%s%s* *%s%s%s* *%s%s%s* *%s%s%s* *%s%s%s* *%s%s%s*\n%s\n",
 
-        landmark[56],
-		landmark[36], landmark[39], landmark[42], landmark[45], landmark[48], landmark[51],
-        landmark[37], landmark[40], landmark[43], landmark[46], landmark[49], landmark[52],
-        landmark[38], landmark[41], landmark[44], landmark[47], landmark[50], landmark[53],
-		landmark[56]);
+        landmark[91],
+		landmark[70], landmark[65], landmark[60], landmark[55], landmark[50], landmark[45],
+        landmark[71], landmark[66], landmark[61], landmark[56], landmark[51], landmark[46],
+        landmark[72], landmark[73], landmark[74], landmark[67], landmark[68], landmark[69], landmark[62], landmark[63], landmark[64], landmark[57], landmark[58], landmark[59], landmark[52], landmark[53], landmark[54], landmark[47], landmark[48], landmark[49],
+		landmark[91]);
 }
 
 void printPlayerLocation(int p1Location, int p2Location)
 {
-	if(p1Location == 0 && p2Location != 0)
-		landmark[53] = " 1    ";
-	else if(p1Location == 1 && p2Location != 1)
-		landmark[50] = " 1    ";
-	else if(p1Location == 2 && p2Location != 2)
-		landmark[47] = " 1    ";
-	else if(p1Location == 3 && p2Location != 3)
-		landmark[44] = " 1    ";
-	else if(p1Location == 4 && p2Location != 4)
-		landmark[41] = " 1    ";
-	else if(p1Location == 5 && p2Location != 5)
-		landmark[38] = " 1    ";
-	else if(p1Location == 6 && p2Location != 6)
-		landmark[32] = " 1    ";
-	else if(p1Location == 7 && p2Location != 7)
-		landmark[26] = " 1    ";
-	else if(p1Location == 8 && p2Location != 8)
-		landmark[20] = " 1    ";
-	else if(p1Location == 9 && p2Location != 9)
-		landmark[2] = " 1    ";
-	else if(p1Location == 10 && p2Location != 10)
-		landmark[5] = " 1    ";
-	else if(p1Location == 11 && p2Location != 11)
-		landmark[8] = " 1    ";
-	else if(p1Location == 12 && p2Location != 12)
-		landmark[11] = " 1    ";
-	else if(p1Location == 13 && p2Location != 13)
-		landmark[14] = " 1    ";
-	else if(p1Location == 14 && p2Location != 14)
-		landmark[17] = " 1    ";
-	else if(p1Location == 15 && p2Location != 15)
-		landmark[23] = " 1    ";
-	else if(p1Location == 16 && p2Location != 16)
-		landmark[29] = " 1    ";
-	else if(p1Location == 17 && p2Location != 17)
-		landmark[35] = " 1    ";
-		
-	if(p2Location == 0 && p1Location != 0)
-		landmark[53] = "    2 ";
-	else if(p2Location == 1 && p1Location != 1)
-		landmark[50] = "    2 ";
-	else if(p2Location == 2 && p1Location != 2)
-		landmark[47] = "    2 ";
-	else if(p2Location == 3 && p1Location != 3)
-		landmark[44] = "    2 ";
-	else if(p2Location == 4 && p1Location != 4)
-		landmark[41] = "    2 ";
-	else if(p2Location == 5 && p1Location != 5)
-		landmark[38] = "    2 ";
-	else if(p2Location == 6 && p1Location != 6)
-		landmark[32] = "    2 ";
-	else if(p2Location == 7 && p1Location != 7)
-		landmark[26] = "    2 ";
-	else if(p2Location == 8 && p1Location != 8)
-		landmark[20] = "    2 ";
-	else if(p2Location == 9 && p1Location != 9)
-		landmark[2] = "    2 ";
-	else if(p2Location == 10 && p1Location != 10)
-		landmark[5] = "    2 ";
-	else if(p2Location == 11 && p1Location != 11)
-		landmark[8] = "    2 ";
-	else if(p2Location == 12 && p1Location != 12)
-		landmark[11] = "    2 ";
-	else if(p2Location == 13 && p1Location != 13)
-		landmark[14] = "    2 ";
-	else if(p2Location == 14 && p1Location != 14)
-		landmark[17] = "    2 ";
-	else if(p2Location == 15 && p1Location != 15)
-		landmark[23] = "    2 ";
-	else if(p2Location == 16 && p1Location != 16)
-		landmark[29] = "    2 ";
-	else if(p2Location == 17 && p1Location != 17)
-		landmark[35] = "    2 ";
-		
-	if(p2Location == 0 && p1Location == 0)
-		landmark[53] = " 1  2 ";
-	else if(p2Location == 1 && p1Location == 1)
-		landmark[50] = " 1  2 ";
-	else if(p2Location == 2 && p1Location == 2)
-		landmark[47] = " 1  2 ";
-	else if(p2Location == 3 && p1Location == 3)
-		landmark[44] = " 1  2 ";
-	else if(p2Location == 4 && p1Location == 4)
-		landmark[41] = " 1  2 ";
-	else if(p2Location == 5 && p1Location == 5)
-		landmark[38] = " 1  2 ";
-	else if(p2Location == 6 && p1Location == 6)
-		landmark[32] = " 1  2 ";
-	else if(p2Location == 7 && p1Location == 7)
-		landmark[26] = " 1  2 ";
-	else if(p2Location == 8 && p1Location == 8)
-		landmark[20] = " 1  2 ";
-	else if(p2Location == 9 && p1Location == 9)
-		landmark[2] = " 1  2 ";
-	else if(p2Location == 10 && p1Location == 10)
-		landmark[5] = " 1  2 ";
-	else if(p2Location == 11 && p1Location == 11)
-		landmark[8] = " 1  2 ";
-	else if(p2Location == 12 && p1Location == 12)
-		landmark[11] = " 1  2 ";
-	else if(p2Location == 13 && p1Location == 13)
-		landmark[14] = " 1  2 ";
-	else if(p2Location == 14 && p1Location == 14)
-		landmark[17] = " 1  2 ";
-	else if(p2Location == 15 && p1Location == 15)
-		landmark[23] = " 1  2 ";
-	else if(p2Location == 16 && p1Location == 16)
-		landmark[29] = " 1  2 ";
-	else if(p2Location == 17 && p1Location == 17)
-		landmark[35] = " 1  2 ";
+	landmark[p1Location * 5 + 2] = " 1";
+	landmark[p2Location * 5 + 4] = "2 ";
 }
