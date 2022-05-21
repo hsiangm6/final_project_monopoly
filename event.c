2847ddd473dcfd1655b4f909023529936c702230
kh010909 <95903086+kh010909@users.noreply.github.com>
2022-05-21 12:56:33 +0800

    Update event.c

--- event.c
+++ event.c
@@ -1,5 +1,16 @@
 #include "event.h"
 
 int event(Player* p1){
-    switch(p1->)
+    switch(p1->location){
+        case 5://jail
+            p1->gamestatus = -1 ;
+        case 2:
+        case 11://draw cards
+            draw();
+        case 9://casino
+            casino();
+        case 14://go to start
+            p1->location = 0;
+            p1->money += 10000;
+    }
 }
