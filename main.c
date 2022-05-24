aaef7cdb5fa63e572223afb1661e2e766c1458a9
HandsomeRay111 <62639431+HandsomeRay111@users.noreply.github.com>
2022-05-24 23:05:24 +0800

    Update main.c

--- main.c
+++ main.c
@@ -56,9 +56,9 @@ int main()
 	char nameOfPlayer1[101], nameOfPlayer2[101];
 	char game_switch[2]="y";
 	puts("Please enter the name of player 1: ");
-    fgets(nameOfPlayer1,100,stdin);
-    puts("Please enter the name of player 2: ");
-	fgets(nameOfPlayer2,100,stdin);
+    	fgets(nameOfPlayer1, 100, stdin);
+    	puts("Please enter the name of player 2: ");
+	fgets(nameOfPlayer2, 100, stdin);
 	
 	Player p1 =
 	{
@@ -144,22 +144,22 @@ void printLand()
 	printf("%s\n*%s* *%s* *%s* *%s* *%s* *%s*\n*%s* *%s* *%s* *%s* *%s* *%s*\n*%s%s%s* *%s%s%s* *%s%s%s* *%s%s%s* *%s%s%s* *%s%s%s*\n%s\n",
 
         landmark[91],
-		landmark[0], landmark[5], landmark[10], landmark[15], landmark[20], landmark[25],
+	landmark[0], landmark[5], landmark[10], landmark[15], landmark[20], landmark[25],
         landmark[1], landmark[6], landmark[11], landmark[16], landmark[21], landmark[26],
         landmark[2], landmark[3], landmark[4], landmark[7], landmark[8], landmark[9], landmark[12], landmark[13], landmark[14], landmark[17], landmark[18], landmark[19], landmark[22], landmark[23], landmark[24], landmark[27], landmark[28], landmark[29],
-		landmark[91]);
+	landmark[91]);
 
-   		printf("%s\n*%s*%s*%s*\n*%s*%s*%s*\n*%s%s%s*%s*%s%s%s*\n%s\n", landmark[92], landmark[85], landmark[90], landmark[30], landmark[86], landmark[90], landmark[31], landmark[87], landmark[88], landmark[89], landmark[90], landmark[32], landmark[33], landmark[34], landmark[92]);
+   	printf("%s\n*%s*%s*%s*\n*%s*%s*%s*\n*%s%s%s*%s*%s%s%s*\n%s\n", landmark[92], landmark[85], landmark[90], landmark[30], landmark[86], landmark[90], landmark[31], landmark[87], landmark[88], landmark[89], landmark[90], landmark[32], landmark[33], landmark[34], landmark[92]);
     	printf("%s\n*%s*%s*%s*\n*%s*%s*%s*\n*%s%s%s*%s*%s%s%s*\n%s\n", landmark[92], landmark[80], landmark[90], landmark[35], landmark[81], landmark[90], landmark[36], landmark[82], landmark[83], landmark[84], landmark[90], landmark[37], landmark[38], landmark[39], landmark[92]);
     	printf("%s\n*%s*%s*%s*\n*%s*%s*%s*\n*%s%s%s*%s*%s%s%s*\n%s\n", landmark[92], landmark[75], landmark[90], landmark[40], landmark[76], landmark[90], landmark[41], landmark[77], landmark[78], landmark[79], landmark[90], landmark[42], landmark[43], landmark[44], landmark[92]);
 
     	printf("%s\n*%s* *%s* *%s* *%s* *%s* *%s*\n*%s* *%s* *%s* *%s* *%s* *%s*\n*%s%s%s* *%s%s%s* *%s%s%s* *%s%s%s* *%s%s%s* *%s%s%s*\n%s\n",
 
         landmark[91],
-		landmark[70], landmark[65], landmark[60], landmark[55], landmark[50], landmark[45],
+	landmark[70], landmark[65], landmark[60], landmark[55], landmark[50], landmark[45],
         landmark[71], landmark[66], landmark[61], landmark[56], landmark[51], landmark[46],
         landmark[72], landmark[73], landmark[74], landmark[67], landmark[68], landmark[69], landmark[62], landmark[63], landmark[64], landmark[57], landmark[58], landmark[59], landmark[52], landmark[53], landmark[54], landmark[47], landmark[48], landmark[49],
-		landmark[91]);
+	landmark[91]);
 }
 
 void printPlayerLocation(int p1Location, int p2Location)
