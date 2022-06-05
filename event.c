f9977556154e5ee9d8ca7f4cc80e2e75c704e84c
hsiangm6--筆電 <hsiangm6@gmail.com>
2022-06-06 00:22:55 +0800

    final

--- event.c
+++ event.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-#include <conio.h>
+//#include <conio.h>
 
 //activity in monopoly
 void event(Player* player, Player* receive, Building *b, char **landmark, char **building)
